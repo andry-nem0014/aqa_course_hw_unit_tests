0b07f0478c70b0e0fbe994ecbbc00c4e69c12947
andry <andry1nemtsev@gmail.com>
2025-07-25 12:42:04 +0300

    edit hw-3

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -4,3 +4,14 @@
 n + nn + nnn, где n не перемножаются, а конкатенируются
 
 */
+const n = 2;
+const sumConcatination = (number) => {
+  if (n >= 1 && n <= 9 && n % 1 === 0) {
+    console.log(
+      String(n) + String(number * 10 + number) + String(number * 100 + number * 10 + number),
+    );
+  } else {
+    console.log('Передайте целое число от 1 до 9!');
+  }
+};
+sumConcatination(n);
