111128d6fac875082dc8fded0e85232c714135cf
andry <andry1nemtsev@gmail.com>
2025-07-30 09:11:39 +0300

    hw-4

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,20 @@
   преобразовываясь в number
 
 */
+
+  let minAge = 18;
+  let maxAge = 60;
+  let age = 25;
+  if (typeof age !== 'number') {
+    Number(age);
+  }
+
+  if (age < minAge) {
+    console.log("You don't have access cause your age is " + age + " It's less then ")
+  } else if (age >= minAge && age < maxAge) {
+    console.log("Welcome  !")
+  } else if (age > maxAge){
+    console.log("Keep calm and look Culture channel")
+  } else {
+    console.log("Technical work")
+  }
